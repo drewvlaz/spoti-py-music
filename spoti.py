e90d303d381e17169c722e1e3b57d4712b41882b
Drew Vlasnik <314dsv@gmail.com>
2020-10-28 12:01:42 -0400

    Added playlists

--- spoti.py
+++ spoti.py
@@ -1,17 +1,21 @@
 from playlist import Playlist
 
+
 def main():
     playlists = {
-        "Just Good Music":'37i9dQZF1DX0b1hHYQtJjp',
-        "Today\'s Top Hits":'37i9dQZF1DXcBWIGoYBM5M',
-        "Power Workout":'37i9dQZF1DWUVpAXiEPK8P',
-        "Hot Rhythmic":'37i9dQZF1DWYs83FtTMQFw',
-        "Rap Caviar":'37i9dQZF1DX0XUsuxWHRQd'
+        "Just Good Music": '37i9dQZF1DX0b1hHYQtJjp',
+        "Today\'s Top Hits": '37i9dQZF1DXcBWIGoYBM5M',
+        "Power Workout": '37i9dQZF1DWUVpAXiEPK8P',
+        "Hot Rhythmic": '37i9dQZF1DWYs83FtTMQFw',
+        "Rap Caviar": '37i9dQZF1DX0XUsuxWHRQd',
+        "late nite - sad times": '7IeEWVi4eLAYGhz4Aj0hpp',
+        "2000s Throwbacks": '5tW8T4fK7DoTtLr8ordLpa'
     }
 
-    selection = "Rap Caviar"
+    selection = "2000s Throwbacks"
     pl = Playlist(selection, playlists[selection])
     pl.get_playlist()
     pl.download_songs()
+
 
 main()
